d63bf11bf787a059493aa91dee9035fcbbafda0b
wesleysmyth <wesleytatesmith@gmail.com>
2024-08-02 13:21:41 -0400

    Remove unnecessary script tag

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,7 +1,13 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
